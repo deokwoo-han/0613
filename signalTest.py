a11faeac5b79542887c9512925ed1009ccafdad9
deokwoo-han <gksejrdn7@gmail.com>
2022-06-13 16:04:08 +0900

    06

--- signalTest.py
+++ signalTest.py
@@ -21,13 +21,21 @@ class MyWindow(QWidget):
         vbox = QVBoxLayout()
         vbox.addWidget(lcd)
         vbox.addWidget(dial)
-        self.setLayout(vbox)
+        vbox.addLayout(hbox)
         self.setLayout(vbox)
 
         dial.valueChanged.connect(lcd.display)
+        btn1.clicked.connect(self.resizeBig)
+        btn2.clicked.connect(self.resizeSmall)
 
         self.setGeometry(50,50,200,200)
         self.show()
+
+    def resizeBig(self):
+        self.resize(400,400)
+
+    def resizeSmall(self):
+        self.resize(150,150)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
